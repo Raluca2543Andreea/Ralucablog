e21070fa077f5628381ca0c922e921fa9aa8acc0
Raluca2543Andreea <159617186+Raluca2543Andreea@users.noreply.github.com>
2024-02-17 22:36:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function openarticles(){
+    window.location.href= "index.html"
+}
+function opencontact(){
+    window.location.href= "contact.html"
+}
+function openreviews(){
+    window.location.href= "reviews.html"
+}
+
+function openuser(){
+    window.location.href= "user.html"
+}
+
